41741023f5e6d5efd26d8a3bbf684af8d6cce06f
Tushitha-Dosapati <tushitha2001@gmail.com>
2026-03-20 00:58:06 -0500

    updated java file

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Git Practice");
         System.out.println("Second change added");
+     
     }
 }
