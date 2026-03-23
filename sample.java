b8bef4d09e7f0c794b21af8da753e0766ef9a5c1
Tushitha-Dosapati <tushitha2001@gmail.com>
2026-03-23 10:55:43 -0500

    Added the variable to the file

--- sample.java
+++ sample.java
@@ -1,6 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
+        int a;
         System.out.println("Hello Git Practice");
          System.out.println("Hello Practice");
+
     }
 }
