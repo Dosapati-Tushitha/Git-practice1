e506c94aeabe7295c1faddf4b95cbabf32e594ad
Tushitha-Dosapati <tushitha2001@gmail.com>
2026-03-23 11:07:57 -0500

    remove nexted repo and added the files properly

---
+++ Git-practice1/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Git Practice");
+        System.out.println("Second change added");
+        System.out.println("third change added");
+     
+    }
+}
