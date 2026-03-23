a9df0e5fc963620bbf04aa8a9c873a033c799b6d
Tushitha-Dosapati <tushitha2001@gmail.com>
2026-03-23 10:56:59 -0500

    variable 2 is added

--- sample.java
+++ sample.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         int a;
+        int b;
         System.out.println("Hello Git Practice");
          System.out.println("Hello Practice");
 
