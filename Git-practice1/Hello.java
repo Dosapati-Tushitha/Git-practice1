6751ed799650ab2ed8713bd1b8b8f21e948b75f9
Tushitha-Dosapati <tushitha2001@gmail.com>
2026-03-23 11:58:23 -0500

    Removed nested folder files

--- Git-practice1/Hello.java
+++
@@ -1,8 +0,0 @@
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello Git Practice");
-        System.out.println("Second change added");
-        System.out.println("third change added");
-     
-    }
-}
--- Git-practice1/README.md
+++
@@ -1,6 +0,0 @@
-# Git-practice1
-This is my Git practice assignment
-Added first change in README
-Added second change for practicegit git log 
-added third change in readme 
-added fourth line 
